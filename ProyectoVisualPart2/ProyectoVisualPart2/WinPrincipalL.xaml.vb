a64fc58270edb8a8e8b3f9033cc8efa0f6403815
yandersantana <ysantanatavares.1996@hotmail.com>
2016-08-12 12:51:14 -0700

    Ventanas enlazadas

---
+++ ProyectoVisualPart2/ProyectoVisualPart2/WinPrincipalL.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class WinPrincipalL
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub btn_aceptar_Click(sender As Object, e As RoutedEventArgs) Handles btn_aceptar.Click
+        Dim winAd As New winAdministrador
+        winAd.Owner = Me
+        winAd.Show()
+        Me.Hide()
+    End Sub
+End Class
--- ProyectoVisualPart2/ProyectoVisualPart2/winAdministrador.xaml.vb
+++ ProyectoVisualPart2/ProyectoVisualPart2/winAdministrador.xaml.vb
@@ -1,3 +1,9 @@
 ﻿Public Class winAdministrador
+    Private Sub btnListaProductos_Click(sender As Object, e As RoutedEventArgs) Handles btnListaProductos.Click
 
+    End Sub
+
+    Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
+        Me.Owner.Show()
+    End Sub
 End Class
