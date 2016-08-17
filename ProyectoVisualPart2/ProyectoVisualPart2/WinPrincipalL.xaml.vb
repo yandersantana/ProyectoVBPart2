91bf669e9a84806641f12a72cd69d716e8cbd961
yandersantana <ysantanatavares.1996@hotmail.com>
2016-08-17 12:58:52 -0500

    validacion

--- ProyectoVisualPart2/ProyectoVisualPart2/WinPrincipalL.xaml.vb
+++ ProyectoVisualPart2/ProyectoVisualPart2/WinPrincipalL.xaml.vb
@@ -1,15 +1,41 @@
-﻿Class WinPrincipalL
+﻿Imports System.Data.OleDb
+
+Class WinPrincipalL
     Public loggedIn As Boolean
     Public usuarios As ArrayList
-    Private strPath = "..\..\..\usuarios.xml"
+    Private strPath = "..\..\dataBaseVisual.mdb"
+    Private strConexion As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & strPath
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+
+
 
     End Sub
 
     Private Sub btn_aceptar_Click(sender As Object, e As RoutedEventArgs) Handles btn_aceptar.Click
-        Dim winAd As New winAdministrador
-        winAd.Owner = Me
-        winAd.Show()
-        Me.Hide()
+        Using conexion As New OleDbConnection(strConexion)
+            conexion.Open()
+
+            Dim consulta As String = "SELECT Count(*) FROM usuarios WHERE Usuario = @usuario and Contraseña = @password"
+
+            Dim cmd As New OleDbCommand(consulta, conexion)
+            cmd.Parameters.AddWithValue("@usuario", txtUsuario.Text)
+            cmd.Parameters.AddWithValue("@password", passwordBox.Password)
+
+            Dim i As Integer = CInt(cmd.ExecuteScalar())
+
+            If i = 0 Then
+                MessageBox.Show("No paso la autenticacion")
+
+            Else
+                MessageBox.Show("autenticacion correcta")
+                Dim winAd As New winAdministrador
+                winAd.Owner = Me
+                winAd.Show()
+                Me.Hide()
+            End If
+
+        End Using
+
     End Sub
 End Class
