88954a436d38329c384e1856a44610267185a961
Kehylaicaza <keilacicaza@gmail.com>
2016-08-19 00:06:41 -0500

    intentando guardar vendedores---- operacion fallida.. Yander no hagas XD yander ratatax9

--- ProyectoVisualPart2/ProyectoVisualPart2/Vendedor.vb
+++ ProyectoVisualPart2/ProyectoVisualPart2/Vendedor.vb
@@ -78,99 +78,9 @@ Public Class Vendedor
         Return MyBase.toString() & "   Id:  " & Id & "    Fecha de contrato:     " & FechaDeContrato & "    Contacto:    " & Contacto
     End Function
 
-    Sub New(raiz As XmlNode)
-        Dim arrVendedores As New ArrayList
-        Dim nombre1, apellido1, email1, telefono1, genero1, cedula1, id1, fechaContrato1, contacto1, usuario1, contraseña1 As String
-        Dim edad1 As Integer
-        Dim ven As Vendedor
-        Dim i As Integer = 1
-
-        For Each nodo As XmlNode In raiz.ChildNodes
-            If nodo.Name = "Vendedor" Then
-                For Each atributo As XmlNode In nodo.ChildNodes
-                    Select Case atributo.Name
-                        Case "Nombre"
-                            nombre1 = atributo.InnerText
-                        Case "Apellido"
-                            apellido1 = atributo.InnerText
-                        Case "Edad"
-                            edad1 = atributo.InnerText
-                        Case "Email"
-                            email1 = atributo.InnerText
-                        Case "Telefono"
-                            telefono1 = atributo.InnerText
-                        Case "Genero"
-                            genero1 = atributo.InnerText
-                        Case "Cedula"
-                            cedula1 = atributo.InnerText
-                        Case "Usuario"
-                            usuario1 = atributo.InnerText
-                        Case "Contraseña"
-                            contraseña1 = atributo.InnerText
-                        Case "Id"
-                            id1 = atributo.InnerText
-                        Case "FechaContrato"
-                            fechaContrato1 = atributo.InnerText
-                        Case "Contacto"
-                            contacto1 = atributo.InnerText
-                    End Select
-                Next
-                ven = New Vendedor(nombre1, apellido1, edad1, email1, telefono1, genero1, cedula1, usuario1, contraseña1, id1, fechaContrato1, contacto1)
-                arrVendedores.Add(ven)
-            End If
 
 
-        Next
 
-        For Each vend As Vendedor In arrVendedores
-            Console.WriteLine(vend)
-            Console.WriteLine(" ")
-        Next
-    End Sub
-
-
-    Public Function agregarVendedor(xmldoc As XmlDocument)
-        Dim vendedor As XmlElement = xmldoc.CreateElement("Vendedor")
-        Dim nombre As XmlElement = xmldoc.CreateElement("Nombre")
-        Dim apellido As XmlElement = xmldoc.CreateElement("Apellido")
-        Dim edad As XmlElement = xmldoc.CreateElement("Edad")
-        Dim email As XmlElement = xmldoc.CreateElement("Email")
-        Dim telefono As XmlElement = xmldoc.CreateElement("Telefono")
-        Dim genero As XmlElement = xmldoc.CreateElement("Genero")
-        Dim cedula As XmlElement = xmldoc.CreateElement("Cedula")
-        Dim usuario As XmlElement = xmldoc.CreateElement("Usuario")
-        Dim contraseña As XmlElement = xmldoc.CreateElement("Contraseña")
-        Dim id As XmlElement = xmldoc.CreateElement("Id")
-        Dim fechaContrato As XmlElement = xmldoc.CreateElement("FechaContrato")
-        Dim contacto As XmlElement = xmldoc.CreateElement("Contacto")
-
-        nombre.InnerText = MyBase.Nombre
-        apellido.InnerText = MyBase.Apellido
-        edad.InnerText = MyBase.Edad
-        email.InnerText = MyBase.Email
-        telefono.InnerText = Me.Telefono
-        genero.InnerText = Me.Genero
-        cedula.InnerText = MyBase.CedulaIdentidad
-        usuario.InnerText = Me.Usuario
-        contraseña.InnerText = Me.Contraseña
-        id.InnerText = Me.Id
-        fechaContrato.InnerText = Me.FechaDeContrato
-        contacto.InnerText = Me.Contacto
-
-        vendedor.AppendChild(nombre)
-        vendedor.AppendChild(apellido)
-        vendedor.AppendChild(edad)
-        vendedor.AppendChild(email)
-        vendedor.AppendChild(telefono)
-        vendedor.AppendChild(genero)
-        vendedor.AppendChild(cedula)
-        vendedor.AppendChild(usuario)
-        vendedor.AppendChild(contraseña)
-        vendedor.AppendChild(id)
-        vendedor.AppendChild(fechaContrato)
-        vendedor.AppendChild(contacto)
-        Return vendedor
-    End Function
 
 
 End Class
--- ProyectoVisualPart2/ProyectoVisualPart2/winNewVendedor.xaml.vb
+++ ProyectoVisualPart2/ProyectoVisualPart2/winNewVendedor.xaml.vb
@@ -1,4 +1,11 @@
-﻿Public Class winNewVendedor
+﻿Imports System.Data.OleDb
+
+Public Class winNewVendedor
+
+    Private strPath = "..\..\dataBaseVisual.mdb"
+
+    Private strConexion As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & strPath
+
     Private Sub btnCancel_Click(sender As Object, e As RoutedEventArgs) Handles btnCancel.Click
         'Dim winADM As New winAdministrador
         'winADM.Owner = Me
@@ -11,6 +18,53 @@
         'winADM.Owner = Me
         'winADM.Show()
         'Me.Hide()
+
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+    Private Sub btnRegistrar_Click(sender As Object, e As RoutedEventArgs) Handles btnRegistrar.Click
+        Using conexion As New OleDbConnection(strConexion)
+            conexion.Open()
+            Dim insertar As String = "INSERT INTO usuarios (Id, Nombre , Apellido , Edad , Email , Telefono , Genero , Cedula , Usuario , Contraseña, FechaContrato , Contacto , administrador) VALUES (@id, @name, @alastname, @age, @mail, @phone, @genero, @cedula, @user, @contraseña, @fc, @contacto, @administrador)"
+            '@name, @alastname, @age, @mail, @phone, @genero, @cedula, @user, @contraseña, @fc, @contacto, @administrador
+            Dim cmd As New OleDbCommand(insertar, conexion)
+            cmd.Parameters.Insert("@name", txtNombre.Text)
+            cmd.Parameters.Insert("@name", txtNombre.Text)
+            cmd.Parameters.Insert("@lastName", txtApellido)
+            cmd.Parameters.Insert("@age", txtEdad.Text)
+            cmd.Parameters.Insert("@mail", txtEmail.Text)
+            cmd.Parameters.Insert("@phone", txtTelefono.Text)
+            cmd.Parameters.Insert("@genero", txtGenero.Text)
+            cmd.Parameters.Insert("@id", txtId.Text)
+            cmd.Parameters.Insert("@cedula", txtCedula.Text)
+            cmd.Parameters.Insert("@user", txtUsuario.Text)
+            cmd.Parameters.Insert("@contraseña", txtContraseña.Text)
+            cmd.Parameters.Insert("@fc", txtFC.Text)
+            cmd.Parameters.Insert("@contacto", txtContacto.Text)
+            cmd.Parameters.Insert("@administrador", False)
+
+            ' cmd.Parameters.Insert("@name", txtNombre.Text)
+            ' cmd.Parameters.AddWithValue("@name", txtNombre.Text)
+            'cmd.Parameters.AddWithValue("@lastName", txtApellido)
+            'cmd.Parameters.AddWithValue("@age", txtEdad.Text)
+            'cmd.Parameters.AddWithValue("@mail", txtEmail.Text)
+            'cmd.Parameters.AddWithValue("@phone", txtTelefono.Text)
+            'cmd.Parameters.AddWithValue("@genero", txtGenero.Text)
+            'cmd.Parameters.AddWithValue("@id", txtId.Text)
+            'cmd.Parameters.AddWithValue("@cedula", txtCedula.Text)
+            'cmd.Parameters.AddWithValue("@user", txtUsuario.Text)
+            'cmd.Parameters.AddWithValue("@contraseña", txtContraseña.Text)
+            'cmd.Parameters.AddWithValue("@fc", txtFC.Text)
+            'cmd.Parameters.AddWithValue("@contacto", txtContacto.Text)
+            'cmd.Parameters.AddWithValue("@administrador", False)
+            'cmd.ExecuteNonQuery()
+        End Using
+
+    End Sub
+
+    Private Sub txtId_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtId.TextChanged
 
     End Sub
 End Class
