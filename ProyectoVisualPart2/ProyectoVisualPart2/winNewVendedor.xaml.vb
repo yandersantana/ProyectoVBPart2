2df5b69d81d617c5d46ab2270707706e57b4a256
Kehylaicaza <keilacicaza@gmail.com>
2016-08-20 21:27:55 -0500

    ya se pueden agregar vendedores a la base de datos

--- ProyectoVisualPart2/ProyectoVisualPart2/winNewVendedor.xaml.vb
+++ ProyectoVisualPart2/ProyectoVisualPart2/winNewVendedor.xaml.vb
@@ -28,14 +28,22 @@ Public Class winNewVendedor
         Using conexion As New OleDbConnection(strConexion)
             conexion.Open()
             Dim Insertar As String
-            Insertar = "INSERT INTO usuarios ([Id], [Nombre], [Apellido], [Edad], [Genero]) values ( txtId.Text, txtNombre.Text,
-            txtApellido.Text,txtEdad.Text,txtGenero.Text)"
+            Insertar = "INSERT INTO usuarios ([Id], [Nombre], [Apellido], [Edad], [Genero], [Email], [Telefono], [Cedula], [Usuario], [Contraseña], [FechaContrato], [Contacto], [administrador]) values ( txtId.Text, txtNombre.Text,
+            txtApellido.Text,txtEdad.Text,txtGenero.Text,txtEmail.Text,txtTelefono.Text,txtCedula.Text,txtUsuario.Text,txtContraseña.Text,txtFC.Text,txtContacto.Text,False)"
             Dim cmd As OleDbCommand = New OleDbCommand(Insertar, conexion)
             cmd.Parameters.Add(New OleDbParameter("Id", CType(txtId.Text, String)))
             cmd.Parameters.Add(New OleDbParameter("Nombre", CType(txtNombre.Text, String)))
             cmd.Parameters.Add(New OleDbParameter("Apellido", CType(txtApellido.Text, String)))
             cmd.Parameters.Add(New OleDbParameter("Edad", CType(txtEdad.Text, String)))
             cmd.Parameters.Add(New OleDbParameter("Genero", CType(txtGenero.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Email", CType(txtEmail.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Telefono", CType(txtTelefono.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Cedula", CType(txtCedula.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Usuario", CType(txtUsuario.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Contraseña", CType(txtContraseña.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("FechaContrato", CType(txtFC.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Contacto", CType(txtContacto.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("administrador", False))
             cmd.ExecuteNonQuery()
 
 
