aff110588dc668fa5950e6f3e5507c187da31f55
Kehylaicaza <keilacicaza@gmail.com>
2016-08-20 21:16:13 -0500

    Problema resuleto nice :v yander ratatax1.1

--- ProyectoVisualPart2/ProyectoVisualPart2/winNewVendedor.xaml.vb
+++ ProyectoVisualPart2/ProyectoVisualPart2/winNewVendedor.xaml.vb
@@ -27,39 +27,20 @@ Public Class winNewVendedor
     Private Sub btnRegistrar_Click(sender As Object, e As RoutedEventArgs) Handles btnRegistrar.Click
         Using conexion As New OleDbConnection(strConexion)
             conexion.Open()
-            Dim insertar As String = "INSERT INTO usuarios (Id, Nombre , Apellido , Edad , Email , Telefono , Genero , Cedula , Usuario , Contraseña, FechaContrato , Contacto , administrador) VALUES (@id, @name, @alastname, @age, @mail, @phone, @genero, @cedula, @user, @contraseña, @fc, @contacto, @administrador)"
-            '@name, @alastname, @age, @mail, @phone, @genero, @cedula, @user, @contraseña, @fc, @contacto, @administrador
-            Dim cmd As New OleDbCommand(insertar, conexion)
-            cmd.Parameters.Insert("@name", txtNombre.Text)
-            cmd.Parameters.Insert("@name", txtNombre.Text)
-            cmd.Parameters.Insert("@lastName", txtApellido)
-            cmd.Parameters.Insert("@age", txtEdad.Text)
-            cmd.Parameters.Insert("@mail", txtEmail.Text)
-            cmd.Parameters.Insert("@phone", txtTelefono.Text)
-            cmd.Parameters.Insert("@genero", txtGenero.Text)
-            cmd.Parameters.Insert("@id", txtId.Text)
-            cmd.Parameters.Insert("@cedula", txtCedula.Text)
-            cmd.Parameters.Insert("@user", txtUsuario.Text)
-            cmd.Parameters.Insert("@contraseña", txtContraseña.Text)
-            cmd.Parameters.Insert("@fc", txtFC.Text)
-            cmd.Parameters.Insert("@contacto", txtContacto.Text)
-            cmd.Parameters.Insert("@administrador", False)
+            Dim Insertar As String
+            Insertar = "INSERT INTO usuarios ([Id], [Nombre], [Apellido], [Edad], [Genero]) values ( txtId.Text, txtNombre.Text,
+            txtApellido.Text,txtEdad.Text,txtGenero.Text)"
+            Dim cmd As OleDbCommand = New OleDbCommand(Insertar, conexion)
+            cmd.Parameters.Add(New OleDbParameter("Id", CType(txtId.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Nombre", CType(txtNombre.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Apellido", CType(txtApellido.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Edad", CType(txtEdad.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Genero", CType(txtGenero.Text, String)))
+            cmd.ExecuteNonQuery()
 
-            ' cmd.Parameters.Insert("@name", txtNombre.Text)
-            ' cmd.Parameters.AddWithValue("@name", txtNombre.Text)
-            'cmd.Parameters.AddWithValue("@lastName", txtApellido)
-            'cmd.Parameters.AddWithValue("@age", txtEdad.Text)
-            'cmd.Parameters.AddWithValue("@mail", txtEmail.Text)
-            'cmd.Parameters.AddWithValue("@phone", txtTelefono.Text)
-            'cmd.Parameters.AddWithValue("@genero", txtGenero.Text)
-            'cmd.Parameters.AddWithValue("@id", txtId.Text)
-            'cmd.Parameters.AddWithValue("@cedula", txtCedula.Text)
-            'cmd.Parameters.AddWithValue("@user", txtUsuario.Text)
-            'cmd.Parameters.AddWithValue("@contraseña", txtContraseña.Text)
-            'cmd.Parameters.AddWithValue("@fc", txtFC.Text)
-            'cmd.Parameters.AddWithValue("@contacto", txtContacto.Text)
-            'cmd.Parameters.AddWithValue("@administrador", False)
-            'cmd.ExecuteNonQuery()
+
+
+
         End Using
 
     End Sub
